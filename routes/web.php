a54ef772a45599ccf0e28c8b920ed3800fc477a1
Alif-Tegor-Sf <aliffarisky1717@gmail>
2022-09-17 14:53:57 +0700

    Membuat Form edit pdf

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
+Route::resource('articles', ArticleController::class);
+
+Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf'])->name('cetak_pdf');
+
+//Auth::routes();
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
